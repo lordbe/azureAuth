630bbd89d4ddea804dd41cc6ff2fe2d36ecb6418
lex <comlar@gmail.com>
2024-05-22 10:48:25 -0400

    help

--- src/cnxgraph.js
+++ src/cnxgraph.js
@@ -11,6 +11,7 @@ import Examplecomponetn from "./examplecomponetn"
         authority: "https://login.microsoftonline.com/d47f94f9-c2b2-4509-840c-d19cd63c7ba8", // Replace with your tenant instance
         redirectUri: "https://azure-auth-nine.vercel.app/", //"http://localhost:8080/redirect", // Replace with your redirect URI
         scopes: ["user.read"], // Scopes for the desired Microsoft Graph access
+        
       },
       cache: {
         cacheLocation: "sessionStorage", // or "localStorage"
